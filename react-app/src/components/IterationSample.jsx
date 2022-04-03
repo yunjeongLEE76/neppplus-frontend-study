67040dc22ca077e8512ddb0c6c9354a7b4bfd9b9
yunjeongLEE76 <98930874+yunjeongLEE76@users.noreply.github.com>
2022-04-03 20:43:09 +0900

    ㅇㅇ

--- react-app/src/components/IterationSample.jsx
+++ react-app/src/components/IterationSample.jsx
@@ -1,27 +1,38 @@
 import { useState } from "react";
 
 const IterationSample = () => {
-  const [names, setNames] = useState(["눈사람", "얼음", "눈", "바람"]);
-  const [message, setMessage] = useState("");
+  const [names, setNames] = useState([
+    { id: 1, text: "눈사람" },
+    { id: 2, text: "얼음" },
+    { id: 3, text: "눈" },
+    { id: 4, text: "바람" },
+  ]);
+
+  const [text, setText] = useState("");
+
+  const [nextId, setNextId] = useState(5);
 
   const handleClick = () => {
-    const newNames = [...names, message];
+    const newNames = [...names, { id: nextId, text: text }];
 
     setNames(newNames);
-    setMessage("");
+    setText("");
+    setNextId(nextId + 1);
   };
+
+  const handleDelete = () => {};
   const handleChange = (e) => {
-    setMessage(e.target.value);
+    setText(e.target.value);
   };
 
   return (
     <div>
-      <input onChange={handleChange} name="message" />
+      <input onChange={handleChange} name="text" />
       <button onClick={handleClick}>추가</button>
       <ul>
         {names.map((name, index) => (
-          <li key={index}>
-            {index}: {name}
+          <li key={name.id} onDobleClick={() => handleDelete(name.id)}>
+            {name.id}: {name.text}
           </li>
         ))}
       </ul>
